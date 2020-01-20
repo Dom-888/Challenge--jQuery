d6e69fd6115257ecdb97efae67f1f552d3028423
Dom-888 <spinelladomenico88@gmail.com>
2020-01-20 20:39:13 +0000

    Add missing rows, removed unused class

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+    // Add missing rows
+    $("table").append("<tr><th>Carrots</th><td>France</td><td>56</td><td>75</td><td>Veg</td></tr>")
+    $("table").append("<tr><th>Carrots</th><td>France</td><td>56</td><td>75</td><td>Veg</td></tr>")
+    $("table").append("<tr><th>Carrots</th><td>France</td><td>56</td><td>75</td><td>Veg</td></tr>")
+
+    // Set rows color
     $("tr:nth-child(even)").addClass("crab_orange")
     $("tr:nth-child(odd)").addClass ("ancient_pink");
 });
