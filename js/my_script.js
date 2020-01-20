8bdb637b2a4bc431758f496e21485c3feb0d1660
Dom-888 <54354612+Dom-888@users.noreply.github.com>
2020-01-20 21:21:57 +0000

    Edit new rows

--- js/my_script.js
+++ js/my_script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
     // Add missing rows
-    $("table").append("<tr><th>Carrots</th><td>France</td><td>56</td><td>75</td><td>Veg</td></tr>")
-    $("table").append("<tr><th>Carrots</th><td>France</td><td>56</td><td>75</td><td>Veg</td></tr>")
-    $("table").append("<tr><th>Carrots</th><td>France</td><td>56</td><td>75</td><td>Veg</td></tr>")
+    $("table").append("<tr><th>Broccoli</th><td>Bulgaria</td><td>12</td><td>70</td><td>Veg</td></tr>")
+    $("table").append("<tr><th>Orange</th><td>Morocco</td><td>15</td><td>50</td><td>Fruit</td></tr>")
+    $("table").append("<tr><th>Tangerine</th><td>Cyprus</td><td>12</td><td>25</td><td>Fruit</td></tr>")
 
     // Set rows color
     $("tr:nth-child(even)").addClass("crab_orange")
